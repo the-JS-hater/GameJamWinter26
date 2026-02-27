cf4f66e30682b5d4fdc6b7cfec76d334f46266e3
Ola Eklund <ola.eklund@live.se>
2026-02-27 22:08:10 +0100

    start editor

---
+++ editor/map_editor.cpp
@@ -0,0 +1,134 @@
+#include <raylib.h>
+#include <raymath.h>
+#include <vector>
+#include <string>
+#include <iostream>
+#include <fstream>
+
+#include "render.hpp"
+
+enum class TileType {
+  EMPTY,
+  GROUND
+};
+
+std::ostream& operator<<(std::ostream& os, const TileType& tile) {
+  return os << (int)tile;
+}
+std::istream& operator>>(std::istream& is, TileType& tile) {
+  int tile_type;
+  is >> tile_type;
+  tile = (TileType)tile_type; 
+  return is;
+}
+
+struct Map {
+  std::vector<std::vector<TileType>> tiles;
+  int width, height;
+
+  Map() {
+    
+  }
+
+  Map(int width, int height) : width(width), height(height) {
+    for (int y = 0; y < width; y++) {
+      std::vector<TileType> row;
+      for (int x = 0; x < height; x++) {
+        row.push_back(TileType::EMPTY);
+      }
+      tiles.push_back(row);
+    }
+  }
+
+  Map(std::string file_name) {
+    std::ifstream file;
+    file.open(file_name);
+    file >> width;
+    file >> height;
+    for (int y = 0; y < width; y++) {
+      std::vector<TileType> row;
+      for (int x = 0; x < height; x++) {
+        TileType tile;
+        file >> tile;
+        row.push_back(tile);
+      }
+      tiles.push_back(row);
+    }
+    file.close();
+  }
+
+  void set(int x, int y, TileType tile) {
+    tiles[y][x] = tile;
+  }
+
+  TileType get(int x, int y) {
+    return tiles[y][x];
+  }
+
+  void write_to_file(std::string file_name) {
+    std::ofstream file;
+    file.open(file_name);
+    file << width << std::endl;
+    file << height << std::endl;
+    for (int y = 0; y < width; y++) {
+      for (int x = 0; x < height; x++) {
+        file << get(x, y) << " ";
+      }
+      file << std::endl;
+    }
+    file.close();
+  }
+};
+
+int main(int argc, char* argv[])
+{
+  int win_w{1280}, win_h{720};
+  char const *win_title{"TITLE"}; 
+  InitWindow(win_w, win_h, win_title);
+
+  Map map;
+  std::string file_name;
+
+  if (argc == 2) {
+    //Read from file
+    map = Map(argv[1]);
+    file_name = argv[1];
+  }
+  else if (argc == 4) {
+    // Create new file
+    map = Map(std::stoi(argv[1]), std::stoi(argv[2]));
+    file_name = argv[3];
+  }
+  else {
+    std::cout << "Wrong number of args!" << std::endl;
+    return -1;
+  }
+
+  float res_w {1920.0f}, res_h {1080.0f};
+  float game_screen_w{1920.0f}, game_screen_h{1080.0f};
+  RenderTexture2D render_target = LoadRenderTexture(game_screen_w, game_screen_h);
+
+  Camera2D camera {
+    { game_screen_w / 2.0f, game_screen_h / 2.0f }, // Offset
+    { // Camera target (rotation and zoom origin)
+      game_screen_w / 2.0f, 
+      game_screen_h / 2.0f
+    },
+    0.0f, // Camera rotation in degrees
+    1.0f // Camera zoom (scaling) 
+  };
+
+  while(!WindowShouldClose())
+  {
+    float w_input, a_input, s_input, d_input;
+    float const dt = GetFrameTime();
+
+
+    // --- Render --- //
+    render_scene(render_target, camera);
+    render_to_screen(render_target, game_screen_w, game_screen_h);
+  }
+
+  map.write_to_file(file_name);
+  CloseWindow();
+}
