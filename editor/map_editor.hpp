c9d965b05c91217d4a07fb8f1f8a73f6a5f7d496
Ola Eklund <ola.eklund@live.se>
2026-02-28 22:39:31 +0100

    Fix editor

--- editor/map_editor.hpp
+++ editor/map_editor.hpp
@@ -19,13 +19,16 @@ std::istream& operator>>(std::istream& is, TileType& tile);
 struct Map {
   std::vector<std::vector<TileType>> tiles;
   int width, height;
+  int spawn_x, spawn_y;
 
   Map();
   Map(int width, int height);
+  Map(int width, int height, int spawn_x, int spawn_y);
   Map(std::string file_name);
 
   void set(int x, int y, TileType tile);
   TileType get(int x, int y) const;
+  void set_spawn(int x, int y);
 
   void write_to_file(std::string file_name) const;
 };
